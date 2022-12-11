c40421b0ab4fc0773dcb4e8131ae6a2d0fa6f916
EGTDEV <107357899+EGTDEV@users.noreply.github.com>
2022-12-11 11:52:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,16 @@ const img = document.querySelector('img')
 img.ondragstart = () => {
   return false;
 };
+Open = false
+buttonMore = document.querySelector(".Trigger")
+moreContent = document.querySelector(".MoreContent")
+buttonMore.addEventListener("click", function(){
+	if(Open==false){
+		moreContent.style.display="flex"
+		Open=true
+	}
+	else{
+		moreContent.style.display="none"
+		Open=false
+	}
+})
